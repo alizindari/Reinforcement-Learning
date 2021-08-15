18f8d3c2abb2b1c5eb0c4e3013b32ccafabcf40b
ali zindari <ali.zindari77@gmail.com>
2021-08-16 01:46:44 +0430

    policy improvement is uploaded

--- monte_carlo learning/Environment.py
+++ monte_carlo learning/Environment.py
@@ -2,25 +2,30 @@ import numpy as np
 
 class Environment:
     
-    def __init__(self,Map):
+    def __init__(self,Map,random_start=True):
         
         self.MAPS ={
-               '4x4':np.array([['s','f','h','h'],
+               '4x4':np.array([['s','f','f','h'],
+                               ['h','h','f','h'],
                                ['h','f','f','h'],
-                               ['h','h','f','f'],
-                               ['h','h','h','g']])
+                               ['h','f','f','g']])
         }
         
         self.NA = 4
+        self.rand_start = random_start
         self.selected_map = self.MAPS[Map]
-        self.start_position = [0,0]
+        self.start_position = (0,0)
         self.goal_position  = [3,3]
         self.current_position = [0,0]
         self.actions = ['r','l','d','u']
         
     def reset(self):
-        self.current_position = [0,0]
-        return self.current_position.copy()
+        if self.rand_start == False:
+            self.current_position = [0,0]
+            return tuple(self.current_position.copy())
+        else:
+            self.current_position = [np.random.randint(0,4),np.random.randint(0,4)]
+            return tuple(self.current_position.copy())
     
     def randomReset(self):
         self.current_position[0] = np.random.randint(0,4)
@@ -49,11 +54,11 @@ class Environment:
             flag = True
             
         if self.getState() == 'g':
-            return [self.current_position[0],self.current_position[1]],10,flag
+            return (self.current_position[0],self.current_position[1]),10,flag
         elif self.getState() == 'h':
-            return [self.current_position[0],self.current_position[1]],-5,flag
+            return (self.current_position[0],self.current_position[1]),-5,flag
         elif self.getState() == 'f' or self.getState() == 's':
-            return [self.current_position[0],self.current_position[1]],0,flag
+            return (self.current_position[0],self.current_position[1]),0,flag
             
     def setPosition(self,x,y):
         self.current_position[0] = x
